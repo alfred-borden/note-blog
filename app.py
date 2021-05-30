94d24e015403ad5af5a27ae3d2e78d850419d942
alfred-borden <chris.hyatt7@gmail.com>
2021-05-30 16:05:47 -0400

    more styling

--- app.py
+++ app.py
@@ -9,18 +9,18 @@ from werkzeug.exceptions import abort
 
 
 def get_db_connection():
-  conn = sqlite3.connect('database.db')
-  conn.row_factory = sqlite3.Row
-  return conn
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 def get_post(post_id):
-  conn = get_db_connection()
-  post = conn.execute('SELECT * FROM posts WHERE id = ?',
-                      (post_id,)).fetchone()
-  conn.close()
-  if post is None:
-    abort(404)
-  return post
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
@@ -28,10 +28,10 @@ app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route('/')
 def index():
-  conn = get_db_connection()
-  posts = conn.execute('SELECT * FROM posts').fetchall()
-  conn.close()
-  return render_template('index.html', posts=posts)
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
 
 
 @app.route('/<int:post_id>')
@@ -57,8 +57,8 @@ def create():
             flash('Title is required!')
         else:
             conn = get_db_connection()
-            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
+            conn.execute('INSERT INTO posts (title, content, content_type) VALUES (?, ?, ?)',
+                         (title, content, content_type))
             conn.commit()
             conn.close()
             return redirect(url_for('index'))
